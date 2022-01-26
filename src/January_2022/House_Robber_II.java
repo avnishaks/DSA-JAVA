5b1445f857d700ab4333048a42f218f4fcccda6c
Avnish Kumar <avnishgolu927@gmail.com>
2022-01-26 23:34:33 +0530

    one more

--- src/January_2022/House_Robber_II.java
+++ src/January_2022/House_Robber_II.java
@@ -1,4 +1,34 @@
 package January_2022;
 
+/*
+1 2 3 1
+
+first include the first house and leave the last
+ 1 2 3 -> part of calculation
+ dp1 -> not include house last and find max
+ 2 3 1 -> part of calculation
+ dp2-> not include house first and find max
+ return the max of dp1 or dp2 .
+ */
+
 public class House_Robber_II {
+    public static int find(int [] nums,int start,int end){
+        int include=0,exclude=0,max=0;
+        for(int i=start;i<end;i++){
+            max=Math.max(include+nums[i],exclude);
+            include=exclude;
+            exclude=max;
+        }
+        return max;
+    }
+    public static int rob(int[] nums){
+        if(nums.length==1){
+            return nums[0];
+        }
+        return Math.max(find(nums,0,nums.length-1),find(nums,1,nums.length));
+    }
+    public static void main(String[] args){
+        int [] nums={1,2,3,1};
+        System.out.println(rob(nums));
+    }
 }
