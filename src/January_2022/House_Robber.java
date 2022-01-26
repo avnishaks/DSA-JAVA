8ed4b4f21a9f502645f21b21d8d05c383843dbbc
Avnish Kumar <avnishgolu927@gmail.com>
2022-01-26 22:00:44 +0530

    one more

--- src/January_2022/House_Robber.java
+++ src/January_2022/House_Robber.java
@@ -1,8 +1,18 @@
 package January_2022;
 
 public class House_Robber {
-
+    public static int rob(int[] nums) {
+        int n=nums.length;
+        int [] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=1;i<n;i++){
+            dp[i+1]=Math.max(dp[i],nums[i]+dp[i-1]);
+        }
+        return dp[n];
+    }
     public static void main(String[] args){
-
+        int [] nums={2,7,9,3,1};
+        System.out.println(rob(nums));
     }
 }
