a599111deeb469e75f1a08bfb813a187890e82b7
Avnish Kumar <avnishgolu927@gmail.com>
2022-01-26 23:44:50 +0530

    one more

--- src/January_2022/House_Robber_II.java
+++ src/January_2022/House_Robber_II.java
@@ -12,20 +12,27 @@ first include the first house and leave the last
  */
 
 public class House_Robber_II {
-    public static int find(int [] nums,int start,int end){
-        int include=0,exclude=0,max=0;
-        for(int i=start;i<end;i++){
-            max=Math.max(include+nums[i],exclude);
-            include=exclude;
-            exclude=max;
-        }
-        return max;
-    }
     public static int rob(int[] nums){
-        if(nums.length==1){
-            return nums[0];
+        int n=nums.length;
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[0],nums[1]);
+
+        // When first house is included , last excluded
+        int [] dp=new int[n+1];
+        dp[1]=nums[0];
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i-1]);
         }
-        return Math.max(find(nums,0,nums.length-1),find(nums,1,nums.length));
+        int include=dp[n-1];
+
+        // When last house included , first included
+        dp=new int[n+1];
+        dp[2]=nums[1];
+        for(int i=3;i<n;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
+
+        return Math.max(dp[n],include);
     }
     public static void main(String[] args){
         int [] nums={1,2,3,1};
