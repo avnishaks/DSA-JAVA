eb512f3e743367a249a7193c9391a41e763111c8
Avnish Kumar <avnishgolu927@gmail.com>
2022-01-27 00:35:33 +0530

    one more

--- src/January_2022/House_Robber_II.java
+++ src/January_2022/House_Robber_II.java
@@ -10,7 +10,7 @@ public class House_Robber_II {
         // When first house is included , last excluded
         int [] dp=new int[n+1];
         dp[1]=nums[0];
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=n;i++){
             dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i-1]);
         }
         int include=dp[n-1];
@@ -18,14 +18,14 @@ public class House_Robber_II {
         // When last house included , first included
         dp=new int[n+1];
         dp[2]=nums[1];
-        for(int i=3;i<n;i++){
+        for(int i=3;i<=n;i++){
             dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i-1]);
         }
 
         return Math.max(dp[n],include);
     }
     public static void main(String[] args){
-        int [] nums={1,2,3,1};
+        int [] nums={1,2,3};
         System.out.println(rob(nums));
     }
 }
---
+++ src/January_2022/delete_and_earn.java
@@ -0,0 +1,31 @@
+package January_2022;
+
+/*
+  Here we have an array of size 10^4
+  so count contain all element from O to 10000
+  then we mention the freq of all the nums element in count array
+  then we take the choice of exclude or include , if we choose x , then we cant choose x-1 or x+1 , we
+  on one case choose a particluar value of x , then we calculate the value , as include and exclude , then
+  at last we return max of include or exclude , i.e will be our answer.
+ */
+public class delete_and_earn {
+    public static int deleteAndEarn(int[] nums) {
+        int include=0,exclude=0;
+        int [] count=new int[10001];
+        for(int i:nums){
+            count[i]++;
+        }
+        for(int i=0;i<10001;i++){
+            int new_include=exclude+count[i]*i;
+            int new_exclude=Math.max(include,exclude);
+            include=new_include;
+            exclude=new_exclude;
+        }
+        return Math.max(include,exclude);
+    }
+    public static void main(String[] args){
+        int [] nums={2,2,3,3,3,4};
+        System.out.println(deleteAndEarn(nums));
+    }
+
+}
