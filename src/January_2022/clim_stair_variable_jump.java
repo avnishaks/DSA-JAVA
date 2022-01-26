9de04b9d9bbed3535220bdac459cee4acc7c5131
Avnish Kumar <avnishgolu927@gmail.com>
2022-01-26 18:47:22 +0530

    2 add

--- src/January_2022/clim_stair_variable_jump.java
+++ src/January_2022/clim_stair_variable_jump.java
@@ -1,2 +1,23 @@
-package January_2022;public class clim_stair_variable_jump {
+package January_2022;
+
+import java.util.Scanner;
+
+public class clim_stair_variable_jump {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int [] dp=new int[n+1];
+
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=arr[i]&&i+j<dp.length;j++){
+                dp[i]+=dp[i+j];
+            }
+        }
+        System.out.println(dp[0]);
+    }
 }
--- src/January_2022/climb_stair.java
+++ src/January_2022/climb_stair.java
@@ -1,2 +1,24 @@
-package January_2022;public class climb_stair {
+package January_2022;
+
+import java.util.Scanner;
+
+public class climb_stair {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] dp=new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<n+1;i++){
+            if(i==1){
+                dp[i]=dp[i-1];
+            }
+            else if(i==2){
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+            else{
+                dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            }
+        }
+        System.out.println(dp[n]);
+    }
 }
