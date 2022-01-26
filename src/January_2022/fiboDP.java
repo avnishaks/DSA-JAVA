a70a6ef59b91d70cc0d03e6afdf0ee633f5db189
Avnish Kumar <avnishgolu927@gmail.com>
2022-01-26 17:18:29 +0530

    Fibo DP

--- src/January_2022/fiboDP.java
+++ src/January_2022/fiboDP.java
@@ -1,2 +1,17 @@
-package January_2022;public class fiboDP {
+package January_2022;
+
+import java.util.Scanner;
+
+public class fiboDP {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
 }
