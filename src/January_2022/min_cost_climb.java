d6b680a29323204ff74f01a6d8d07408fadf973e
Avnish Kumar <avnishgolu927@gmail.com>
2022-01-26 21:28:01 +0530

    new

---
+++ src/January_2022/min_cost_climb.java
@@ -0,0 +1,18 @@
+// 746. Min Cost Climbing Stairs
+package January_2022;
+
+public class min_cost_climb {
+    public static int minCostClimbingStairs(int[] cost) {
+        int s1=0,s2=0,curr_step;
+        for(int i=cost.length-1;i>=0;i--){
+            curr_step=cost[i]+Math.min(s1,s2);
+            s1=s2;
+            s2=curr_step;
+        };
+        return Math.min(s1,s2);
+    }
+    public static void main(String[] args){
+        int cost[]={10,15,20};
+        System.out.println(minCostClimbingStairs(cost));
+    }
+}
