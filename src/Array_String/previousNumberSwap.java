bff4bed901be9b21fc1ef8f84f90656f2c6ffdd2
Avnish Kumar <avnishgolu927@gmail.com>
2022-04-04 04:00:12 +0530

    error resolved

--- src/Array_String/previousNumberSwap.java
+++ src/Array_String/previousNumberSwap.java
@@ -1,38 +1,39 @@
 package Array_String;
 
 public class previousNumberSwap {
-    public static boolean flag=false;
     public static String swap(String s,int i, int j){
         char ch[]=s.toCharArray();
         char temp=ch[i];
         ch[i]=ch[j];
         ch[j]=temp;
-        flag=true;
         return String.valueOf(ch);
     }
     public static String previous(String s){
-        char chArr[];
-        String st="";
-        char ch[]=s.toCharArray();
         int i=s.length()-2;
-        while(i>=0&&ch[i]<=ch[i+1]){
+        while (i>=0&&s.charAt(i)<=s.charAt(i+1)){
             i--;
         }
-        if(i>=0){
-            int j=s.length()-1;
-            while(ch[i]<=ch[j]){
-                j--;
+        if(i==-1)
+            return "-1";
+        int min=Integer.MAX_VALUE;
+        int idx=0;
+        for(int j=i+1;j<s.length();j++)
+        {
+            int d=s.charAt(i)-s.charAt(j);
+            if(d>0 && min>d)
+            {
+                min=d;
+                idx=j;
             }
-            st=String.valueOf(ch);
-            st=swap(st,i,j);
         }
-        if(flag==true&&st.charAt(0)!='0'){
-            return st;
+        s=swap(s,i,idx);
+        if(s.charAt(0)!='0'){
+            return s;
         }
         return "-1";
     }
     public static void main(String[] args) {
-        String s="77";
+        String s="99900000";
         System.out.println(previous(s));
     }
 }
