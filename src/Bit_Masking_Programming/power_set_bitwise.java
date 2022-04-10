2517ae277d7e11d0089c7ea8b8a484a29e2f654c
Avnish Kumar <avnishgolu927@gmail.com>
2022-04-10 17:54:34 +0530

    added one approach

--- src/Bit_Masking_Programming/power_set_bitwise.java
+++ src/Bit_Masking_Programming/power_set_bitwise.java
@@ -1,5 +1,26 @@
 package Bit_Masking_Programming;
 
 public class power_set_bitwise {
+    static void printPowerSet(String str)
+    {
+        int n = str.length();
 
+        int powSize = (int)Math.pow(2, n);
+
+        for(int counter = 0; counter < powSize; counter++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if((counter & (1 << j)) != 0)
+                    System.out.print(str.charAt(j));
+            }
+
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        String s = "abc";
+
+        printPowerSet(s);
+    }
 }
